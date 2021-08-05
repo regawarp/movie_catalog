e679a21e2d8f06f60650513d61865f7fa504cac3
regawarp <regawa04@gmail.com>
2021-08-05 10:01:27 +0700

    Show List of Movies using ListView.Builder

--- lib/main.dart
+++ lib/main.dart
@@ -30,10 +30,67 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Future<List<Movie>> _movieList;
+
   @override
   void initState() {
     super.initState();
-    generateMovieList();
+    _movieList = generateMovieList();
+  }
+
+  Future<List<Movie>> generateMovieList() async {
+    List<Movie> _movieList = List<Movie>();
+    _movieList.add(Movie(
+        "Raya and the Last Dragon",
+        "https://cdn.popmama.com/content-images/post/20210128/raya-and-the-last-dragon-3-5fe68c989c6be4bbb26222c710104f23_600xauto.jpg",
+        2021,
+        7.4,
+        "PG",
+        "1h 57m",
+        "Long ago, in the fantasy world of Kumandra, humans and dragons lived together in harmony. But when an evil force threatened the land, the dragons sacrificed themselves to save humanity. Now, 500 years later, that same evil has returned and it’s up to a lone warrior, Raya, to track down the legendary last dragon to restore the fractured land and its divided people.",
+        "Fantasy",
+        "1VIZ89FEjYI"));
+    _movieList.add(Movie(
+        "Zack Snyder's Justice League",
+        "https://pict.sindonews.net/dyn/600/pena/sindo-article/original/2021/03/03/Zack%20Snyder%20Justice%20League%20-%20Heroes%20Key%20Art%20Vertical.jpg",
+        2021,
+        8.1,
+        "R",
+        "4h 2m",
+        "Determined to ensure Superman's ultimate sacrifice was not in vain, Bruce Wayne aligns forces with Diana Prince with plans to recruit a team of metahumans to protect the world from an approaching threat of catastrophic proportions.",
+        "Action",
+        "vM-Bja2Gy04"));
+    _movieList.add(Movie(
+        "Monster Hunter",
+        "https://i.pinimg.com/564x/3a/4f/c1/3a4fc10dff3393dfdb11ae505d915f93.jpg",
+        2020,
+        5.3,
+        "PG-13",
+        "1h 44m",
+        "A portal transports Cpt. Artemis and an elite unit of soldiers to a strange world where powerful monsters rule with deadly ferocity. Faced with relentless danger, the team encounters a mysterious hunter who may be their only hope to find a way home.",
+        "Fantasy",
+        "3od-kQMTZ9M"));
+    _movieList.add(Movie(
+        "Below Zero",
+        "https://m.media-amazon.com/images/M/MV5BMjg0NTBjOWEtMDc1OS00ZDEyLWE3ZTItMGQzYTIyZDJkM2U2XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+        2021,
+        6.2,
+        "R",
+        "1h 46m",
+        "When a prisoner transfer van is attacked, the cop in charge must fight those inside and outside while dealing with a silent foe: the icy temperatures.",
+        "Action",
+        "hSMokfUerVY"));
+    _movieList.add(Movie(
+        "Tom & Jerry",
+        "https://cdn.cinematerial.com/p/297x/osu2gdkh/tom-and-jerry-movie-poster-md.jpg?v=1614079055",
+        2021,
+        5.3,
+        "PG",
+        "1h 41m",
+        "Tom the cat and Jerry the mouse get kicked out of their home and relocate to a fancy New York hotel, where a scrappy employee named Kayla will lose her job if she can’t evict Jerry before a high-class wedding at the hotel. Her solution? Hiring Tom to get rid of the pesky mouse.",
+        "Family",
+        "kP9TfCWaQT4"));
+    return _movieList;
   }
 
   @override
@@ -42,61 +99,25 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Text(widget.title),
+      body: FutureBuilder(
+        future: _movieList,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error while retrieving data');
+          } else if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('${snapshot.data[index].title}'),
+                );
+              },
+            );
+          } else {
+            return CircularProgressIndicator();
+          }
+        },
+      ),
     );
   }
-}
-
-Future<List<Movie>> generateMovieList() {
-  List<Movie> _movieList = List<Movie>();
-  _movieList.add(Movie(
-      "Raya and the Last Dragon",
-      "",
-      2021,
-      7.4,
-      "PG",
-      "1h 57m",
-      "Long ago, in the fantasy world of Kumandra, humans and dragons lived together in harmony. But when an evil force threatened the land, the dragons sacrificed themselves to save humanity. Now, 500 years later, that same evil has returned and it’s up to a lone warrior, Raya, to track down the legendary last dragon to restore the fractured land and its divided people.",
-      "Fantasy",
-      "1VIZ89FEjYI"));
-  _movieList.add(Movie(
-      "Zack Snyder's Justice League",
-      "",
-      2021,
-      8.1,
-      "R",
-      "4h 2m",
-      "Determined to ensure Superman's ultimate sacrifice was not in vain, Bruce Wayne aligns forces with Diana Prince with plans to recruit a team of metahumans to protect the world from an approaching threat of catastrophic proportions.",
-      "Action",
-      "vM-Bja2Gy04"));
-  _movieList.add(Movie(
-      "Monster Hunter",
-      "R.drawable.poster3",
-      2020,
-      5.3,
-      "PG-13",
-      "1h 44m",
-      "A portal transports Cpt. Artemis and an elite unit of soldiers to a strange world where powerful monsters rule with deadly ferocity. Faced with relentless danger, the team encounters a mysterious hunter who may be their only hope to find a way home.",
-      "Fantasy",
-      "3od-kQMTZ9M"));
-  _movieList.add(Movie(
-      "Below Zero",
-      "",
-      2021,
-      6.2,
-      "R",
-      "1h 46m",
-      "When a prisoner transfer van is attacked, the cop in charge must fight those inside and outside while dealing with a silent foe: the icy temperatures.",
-      "Action",
-      "hSMokfUerVY"));
-  _movieList.add(Movie(
-      "Tom & Jerry",
-      "",
-      2021,
-      5.3,
-      "PG",
-      "1h 41m",
-      "Tom the cat and Jerry the mouse get kicked out of their home and relocate to a fancy New York hotel, where a scrappy employee named Kayla will lose her job if she can’t evict Jerry before a high-class wedding at the hotel. Her solution? Hiring Tom to get rid of the pesky mouse.",
-      "Family",
-      "kP9TfCWaQT4"));
 }
